a408055db7481770766140765db80164a62e7dd2
NBO2001 <natanaelbezerra36@gmail.com>
2023-03-03 21:39:49 -0400

    Tetse

--- main.go
+++ main.go
@@ -6,43 +6,6 @@ import (
 	"fmt"
 )
 
-type Person struct {
-	name   string
-	age    int
-	height float64
-}
-
-func NewPerson(name string, age int, height float64) *Person {
-	return &Person{name, age, height}
-}
-
-func (p *Person) setName(name string) {
-
-	p.name = name
-
-}
-
-func (p *Person) getName() string {
-	return p.name
-}
-
-func (p *Person) setAge(age int) {
-
-	p.age = age
-}
-
-func (p *Person) getAge() int {
-	return p.age
-}
-
-func (p *Person) setHeight(height float64) {
-	p.height = height
-}
-
-func (p *Person) getHeight() float64 {
-	return p.height
-}
-
 func main() {
 	person := NewPerson("Alice", 25, 1.65)
 	fmt.Println(person.getName())
